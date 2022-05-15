df461bba7f2101539a439cd16166eb1a505362c6
Nicolas Wicki <58391419+nwicki@users.noreply.github.com>
2022-05-15 15:09:53 +0200

    Remove build

--- Build2019/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Nicolas\programs\4d-holographic-tutorials\Build2019\Il2CppOutputProject\IL2CPP\libil2cpp\mono-runtime\il2cpp-callbacks.cpp"
-#include "C:\Users\Nicolas\programs\4d-holographic-tutorials\Build2019\Il2CppOutputProject\IL2CPP\libil2cpp\mono-runtime\il2cpp-mono-support.cpp"

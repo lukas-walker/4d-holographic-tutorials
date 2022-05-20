1aceb39bb6143e564c33e75cf2dfe33b3083d0f5
Nicolas Wicki <58391419+nwicki@users.noreply.github.com>
2022-05-20 21:49:24 +0200

    Revert "Objects are instantiated not original - WIP"
    This reverts commit a9183257c6bad8a13b40a3c8e72bcdcf327dcbf0.

--- Assets/Scripts/ObjectControl.cs
+++ Assets/Scripts/ObjectControl.cs
@@ -7,16 +7,7 @@ using UnityEngine;
 /// </summary>
 public class ObjectControl : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject objectModel;
-    [SerializeField]
-    private GameObject objectManagerPanel;
-
-    /// <summary>
-    /// Gives each instance a new ID
-    /// </summary>
-    private int n_clones;
-    
+    public GameObject objectModel;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +15,22 @@ public class ObjectControl : MonoBehaviour
     }
 
     /// <summary>
-    /// Spawns an instance of the assigned object
+    /// Set the position of the spawned object relative to the object manager panel
     /// </summary>
-    public void SpawnObject()
+    public void SetManagerRelativePosition()
     {
-        GameObject clone = Instantiate(
-            objectModel,
-            objectManagerPanel.transform.position + new Vector3(.0f, .1f, .0f),
-            objectManagerPanel.transform.rotation * objectModel.transform.rotation,
-            objectModel.transform.parent);
-        Destroy(clone.GetComponent<ObjectControl>());
-        clone.name = $"{objectModel.name}-{n_clones++}";
-        clone.transform.localScale *= 3;
-        clone.SetActive(true);
+        GameObject panel = GameObject.Find("ObjectManagerPanel");
+        if (panel == null) return;
+
+        objectModel.transform.SetPositionAndRotation(panel.transform.position + new Vector3(.0f, .075f, .0f), panel.transform.rotation * objectModel.transform.rotation);
+    }
+
+    /// <summary>
+    /// Toggles the assigned object
+    /// </summary>
+    public void ToggleObject()
+    {
+        SetManagerRelativePosition();
+        objectModel.SetActive(!objectModel.activeSelf);
     }
 }

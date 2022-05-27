68584f55f853dae08eebd81deb29ddb4c554c3b2
Nicolas Wicki <58391419+nwicki@users.noreply.github.com>
2022-05-27 15:07:21 +0200

    Disable hand mesh at start

--- Assets/Scripts/FeaturesPanelVisuals.cs
+++ Assets/Scripts/FeaturesPanelVisuals.cs
@@ -14,24 +14,17 @@ namespace Microsoft.MixedReality.Toolkit.UI
     {
         [SerializeField]
         private Interactable handMeshButton = null;
-        [SerializeField]
-        private Interactable pointCloudButton = null;
-        [SerializeField]
-        private Interactable objectManagerPanelButton = null;
-        [SerializeField]
-        private Interactable recordingManagerPanelButton = null;
 
         private void Start()
         {
-            MixedRealityHandTrackingProfile handProfile = null;
             if (CoreServices.InputSystem?.InputSystemProfile != null)
             {
-                handProfile = CoreServices.InputSystem.InputSystemProfile.HandTrackingProfile;
+                MixedRealityHandTrackingProfile handProfile = CoreServices.InputSystem.InputSystemProfile.HandTrackingProfile;
+                if (handProfile != null)
+                {
+                    handProfile.EnableHandMeshVisualization = handMeshButton.IsToggled;
+                }
             }
-            handMeshButton.IsToggled = handProfile != null && handProfile.EnableHandMeshVisualization;
-            pointCloudButton.IsToggled = false;
-            objectManagerPanelButton.IsToggled = false;
-            recordingManagerPanelButton.IsToggled = false;
         }
     }
 }
